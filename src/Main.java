fdaac0a573b1ffa14da117f2eb42bc013829761d
админ <rafaberdali1@gmail.com>
2023-07-31 09:40:29 +0600

    task 1

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,27 @@ public class Main {
             return sum / n;
         }
     }
+
+    public static boolean isPrime(int n, int divisor) {
+        if (n < 2) {
+            return false;
+        }
+        if (divisor > Math.sqrt(n)) {
+            return true;
+        }
+        if (n % divisor == 0) {
+            return false;
+        }
+        return isPrime(n, divisor + 1);
+    }
+
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -36,6 +57,27 @@ public class Main {
         // Task 2
         double average = findAverage(array, array.length);
         System.out.println("Average of numbers in the array: " + average);
+
+        // Task 3
+        System.out.print("Enter a number to check if it's prime or composite: ");
+        int numberToCheck = scanner.nextInt();
+        boolean isPrimeNumber = isPrime(numberToCheck, 2);
+        if (isPrimeNumber) {
+            System.out.println(numberToCheck + " is a prime number.");
+        } else {
+            System.out.println(numberToCheck + " is a composite number.");
+        }
+        // Task 4
+        System.out.print("Enter a number to calculate its factorial: ");
+        int number = scanner.nextInt();
+        if (number < 0) {
+            System.out.println("Factorial is not defined for negative numbers.");
+        } else {
+            int result = factorial(number);
+            System.out.println("Factorial of " + number + ": " + result);
+        }
+
+
         scanner.close();
     }
 
