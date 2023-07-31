94f8253bdc9334fe331d75f906108701c794527b
админ <rafaberdali1@gmail.com>
2023-07-31 09:40:29 +0600

    task 1

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,20 @@ public class Main {
             return sum / n;
         }
     }
+
+    public static boolean isPrime(int n, int divisor) {
+        if (n < 2) {
+            return false;
+        }
+        if (divisor > Math.sqrt(n)) {
+            return true;
+        }
+        if (n % divisor == 0) {
+            return false;
+        }
+        return isPrime(n, divisor + 1);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -36,6 +50,19 @@ public class Main {
         // Task 2
         double average = findAverage(array, array.length);
         System.out.println("Average of numbers in the array: " + average);
+
+        // Task 3
+        System.out.print("Enter a number to check if it's prime or composite: ");
+        int numberToCheck = scanner.nextInt();
+        boolean isPrimeNumber = isPrime(numberToCheck, 2);
+        if (isPrimeNumber) {
+            System.out.println(numberToCheck + " is a prime number.");
+        } else {
+            System.out.println(numberToCheck + " is a composite number.");
+        }
+
+
+
         scanner.close();
     }
 
